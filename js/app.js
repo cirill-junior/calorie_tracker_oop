770e7369bb9ef5686ae0b73f662d956799b4afee
cirill <cirill.mladenov@gmail.com>
2023-09-06 05:16:03 +0300

    total track

--- js/app.js
+++ js/app.js
@@ -4,18 +4,34 @@ class CalorieTracker {
 		this._totalCalories = 0;
 		this._meals = [];
 		this._workouts = [];
+
+		this._displayCaloriesTotal();
 	}
 
+	/* Public Мethods/API */
 	addMeal(meal) {
 		this._meals.push(meal);
 		this._totalCalories += meal.calories;
 		this.meal = meal;
+		this._render();
 	}
 	addWorkout(workout) {
 		const meal = this.meal;
 		this._workouts.push(workout);
 		this._totalCalories -= workout.calories;
 		this.workout = workout;
+		this._render();
+	}
+
+	/*  Private Methods */
+
+	_displayCaloriesTotal() {
+		const totalCaloriesEl = document.getElementById('calories-total');
+		totalCaloriesEl.innerHTML = this._totalCalories;
+	}
+
+	_render() {
+		this._displayCaloriesTotal();
 	}
 }
 
@@ -38,7 +54,9 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
 tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
 
 const run = new Workout('run', 300);
 tracker.addWorkout(run);
