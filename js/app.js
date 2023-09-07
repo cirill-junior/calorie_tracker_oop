a1b2723a345e112b26f63d02f14a4e2d31103a84
cirill <cirill.mladenov@gmail.com>
2023-09-07 05:34:03 +0300

    fix bugs on calorieTracker 34 and removeItem 238

--- js/app.js
+++ js/app.js
@@ -17,15 +17,25 @@ class CalorieTracker {
 	addMeal(meal) {
 		this._meals.push(meal);
 		this._totalCalories += meal.calories;
-		this.meal = meal;
+		this._displayNewMeal(meal);
 		this._render();
 	}
 	addWorkout(workout) {
 		const meal = this.meal;
 		this._workouts.push(workout);
 		this._totalCalories -= workout.calories;
-		this.workout = workout;
+		this._displayNewWorkout(workout);
 		this._render();
+	}
+	removeMeal(id) {
+		const index = this._meals.findIndex((meal) => meal.id === id);
+
+		if (index !== -1) {
+			const meal = this._meal[index];
+			this._totalCalories -= meal.calories;
+			this._meals.splice(index, 1);
+			this._render();
+		}
 	}
 
 	/*  Private Methods */
@@ -90,6 +100,54 @@ class CalorieTracker {
 		progressEl.style.width = `${width}%`;
 	}
 
+	_displayNewMeal(meal) {
+		const mealsEl = document.getElementById('meal-items');
+		const mealEl = document.createElement('div');
+		mealEl.classList.add('card', 'my-2');
+		mealEl.setAttribute('data-id', meal.id);
+		mealEl.innerHTML = `
+		<div class="card-body">
+		<div class="d-flex align-items-center justify-content-between">
+		  <h4 class="mx-1">${meal.name}</h4>
+		  <div
+			class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+		  >
+			${meal.calories}
+		  </div>
+		  <button class="delete btn btn-danger btn-sm mx-2">
+			<i class="fa-solid fa-xmark"></i>
+		  </button>
+		</div>
+	  </div>
+	  `;
+
+		mealsEl.appendChild(mealEl);
+	}
+
+	_displayNewWorkout(workout) {
+		const workoutsEl = document.getElementById('workout-items');
+		const workoutEl = document.createElement('div');
+		workoutEl.classList.add('card', 'my-2');
+		workoutEl.setAttribute('data-id', workout.id);
+		workoutEl.innerHTML = `
+		<div class="card-body">
+		<div class="d-flex align-items-center justify-content-between">
+		  <h4 class="mx-1">${workout.name}</h4>
+		  <div
+			class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+		  >
+			${workout.calories}
+		  </div>
+		  <button class="delete btn btn-danger btn-sm mx-2">
+			<i class="fa-solid fa-xmark"></i>
+		  </button>
+		</div>
+	  </div>
+	  `;
+
+		workoutsEl.appendChild(workoutEl);
+	}
+
 	_render() {
 		this._displayCaloriesTotal();
 		this._displayCaloriesConsumed();
@@ -125,6 +183,10 @@ class App {
 		document
 			.getElementById('workout-form')
 			.addEventListener('submit', this._newItem.bind(this, 'workout'));
+
+		document
+			.getElementById('meal-items')
+			.addEventListener('click', this._removeItem.bind(this, 'meal'));
 	}
 
 	_newItem(type, e) {
@@ -162,6 +224,23 @@ class App {
 		const bsCollapse = new bootstrap.Collapse(collapseItem, {
 			toggle: true,
 		});
+	}
+
+	_removeItem(type, e) {
+		if (
+			e.target.classList.contains('delete') ||
+			e.target.classList.contains('fa-xmark')
+		) {
+			if (confirm('Are you sure?')) {
+				const id = e.target.closest('.card').getAttribute('data-id');
+
+				type === 'meal'
+					? this._tracker.removeMeal(id)
+					: this._tracker.removeWorkout(id);
+
+				e.target.closest('card').remove();
+			}
+		}
 	}
 }
 
